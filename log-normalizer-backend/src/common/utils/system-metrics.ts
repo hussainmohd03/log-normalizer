ac85305bfc5d2215ab9156b1467e8b1a828da92e
HussainDT <hussainmohdali03@gmail.com>
2026-04-04 18:23:10 +0300

    fix field name in systemMetrics to match python case

--- log-normalizer-backend/src/common/utils/system-metrics.ts
+++ log-normalizer-backend/src/common/utils/system-metrics.ts
@@ -1,26 +1,24 @@
 import * as os from 'os'
 
 export interface SystemMetrics {
-  memoryUsedMb: number
-  memoryTotalMb: number
-  memoryPercent: number
-  cpuLoadAvg1m: number
-  cpuCores: number
-  uptimeSeconds: number
+  memory_used_mb: number
+  memory_total_mb: number
+  memory_percent: number
+  cpu_load_avg_1m: number
+  cpu_cores: number
+  uptime_seconds: number
 }
 
 export const getSystemMetrics = (): SystemMetrics => {
-  const mem = process.memoryUsage()
   const totalMem = os.totalmem()
-  const freeMem = os.freemem()
-  const usedMem = totalMem - freeMem
+  const usedMem = totalMem - os.freemem()
 
   return {
-    memoryUsedMb: Math.round(usedMem / 1024 / 1024),
-    memoryTotalMb: Math.round(totalMem / 1024 / 1024),
-    memoryPercent: Math.round((usedMem / totalMem) * 100),
-    cpuLoadAvg1m: Math.round(os.loadavg()[0] * 100) / 100,
-    cpuCores: os.cpus().length,
-    uptimeSeconds: Math.floor(process.uptime()),
+    memory_used_mb: Math.round(usedMem / 1024 / 1024),
+    memory_total_mb: Math.round(totalMem / 1024 / 1024),
+    memory_percent: Math.round((usedMem / totalMem) * 100),
+    cpu_load_avg_1m: Math.round(os.loadavg()[0] * 100) / 100,
+    cpu_cores: os.cpus().length,
+    uptime_seconds: Math.floor(process.uptime()),
   }
 }
